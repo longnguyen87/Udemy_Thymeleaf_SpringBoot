62d2baa701aa67ca3e3f454ffd179cda972efbaa
CNTR\c072773 <long.nguyen@thrivent.com>
2020-03-25 09:23:09 -0500

    Initial Project

---
+++ ThymeLeafUdemy/src/main/java/thymeleaf/ThymeLeafUdemyApplication.java
@@ -0,0 +1,13 @@
+package thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeLeafUdemyApplication.class, args);
+	}
+
+}
